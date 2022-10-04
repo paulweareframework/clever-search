14a7c82898b110502775e4a585d5daed23b29212
paulweareframework <paul.q@weareframework.co.uk>
2022-10-04 10:48:06 +0100

    speeling amends

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -17,12 +17,12 @@
 
         @if(isset($values['clever_search_which_collection']) && !is_null($values['clever_search_which_collection']) && !empty($values['clever_search_which_collection']))
             <div class="flex flex-col items-start justify-start mb-3">
-                <p class="mb-1">No we have the settings you can:</p>
+                <p class="mb-1">Now we have the settings you can:</p>
                 <a href="{{ cp_route('weareframework.clever-search.settings.update-search-indexes') }}" class="btn mb-1">Update Search Indexes</a>
-                <p>We have <strong>{{ is_array($values['clever_search_results']) ? count($values['clever_search_results']) : 0 }}</strong> records indexed for search</p>
-                <p>Test the results with the api search results: {{ config('app.url') }}/clever-search/search?q={name,sku,description}&options[includeScore]=true&options[includeMatches]=true</p>
-                <p>The url can be hit with POST or GET. For passing search options, use POST.</p>
-                <p>If you wish to pass options to the search you can with fuse options https://github.com/loilo/Fuse#options</p>
+                <p>We have <strong>{{ is_array($values['clever_search_results']) ? count($values['clever_search_results']) : 0 }}</strong> records indexed for search.</p>
+                <p>Test the results with the api search results: <a href="{{ config('app.url') }}/clever-search/search?q={name,sku,description}&options[includeScore]=true&options[includeMatches]=true" target="_blank">{{ config('app.url') }}/clever-search/search?q={name,sku,description}&options[includeScore]=true&options[includeMatches]=true</a></p>
+                <p>The url can be hit with <strong>POST</strong> or <strong>GET</strong>. For passing search options, use <strong>POST</strong>.</p>
+                <p>If you wish to pass options to the search you can with fuse options <a href="https://github.com/loilo/Fuse#options" target="_blank">https://github.com/loilo/Fuse#options</a></p>
             </div>
         @endif
     </div>
